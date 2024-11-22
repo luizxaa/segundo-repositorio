11808891c084d406efb02594809cdc9637fe1f58
LUIZ <138634276+luizxaa@users.noreply.github.com>
2024-11-22 19:42:31 -0300

    22/11/24 1

--- missaoVisaoValores.js
+++ missaoVisaoValores.js
@@ -1,17 +1,18 @@
 criaCartao (
     'Missão',
     'Saiba qual é a missão de nossa empresa!',
-    'Nossa missão é ...'
+    'Nossa missão é ...Fornecer produtos e serviços de qualidade excepcional para atender às necessidades de nossos clientes de forma eficiente e ética.'
 )
 
 criaCartao (
     'Visão',
     'Saiba qual é a visão de nossa empresa!',
-    'Nossa visão é ...'
+    'Nossa visão é ...Ser reconhecido como líder em nossa empresa, contando com uma equipe comprometida e inovadora, proporcionando crescimento constante.'
 )
 
 criaCartao (
     'Valores',
     'Saiba quais são os valores de nossa empresa!',
-    'Nossos valores são...'
+    'Nossos valores são ....Conduzir negócios de forma
+ 'ética e transparente Buscar melhorias e inovações em nossos produtos Valorizar e desenvolver os colaboradores para alcançarmos nossos objetivos'
 )
